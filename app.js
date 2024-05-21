cd7abebfbec656b611f3d588763c9b84a77360d9
Suvankar Mahato <suvankar_m@pursuitsoftware.com>
2024-05-21 15:54:33 +0530

    Auth update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRoutes from "./routes/user.js";
 import {config} from 'dotenv';
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 export const app=express();
@@ -12,6 +13,7 @@ config({
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 
 app.use("/api/v1/users",userRoutes);
