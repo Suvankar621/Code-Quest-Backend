e44dc545720243edacb8c789db85f7ef32a6e0a5
Suvankar Mahato <suvankar_m@pursuitsoftware.com>
2024-05-22 11:50:27 +0530

    hosting setup

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
+    origin:[process.env.FRONTEND_URL,"http://localhost:3000"],
     credentials:true
 }));
 
