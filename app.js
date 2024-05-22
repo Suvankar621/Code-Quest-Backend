a4d542c0532b7909c684f0a97ac415e605a9dbfe
Suvankar Mahato <suvankar_m@pursuitsoftware.com>
2024-05-22 09:41:58 +0530

    Auth update

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:[process.env.FRONTEND_URL]
+    origin:[process.env.FRONTEND_URL],
+    credentials:true
 }));
 
 
