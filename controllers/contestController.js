e67bda51c2d5a088a8373aa1de1cd854cc6f3b2d
Suvankar Mahato <suvankar_m@pursuitsoftware.com>
2024-05-26 22:06:48 +0530

    Contest Api completed

--- controllers/contestController.js
+++ controllers/contestController.js
@@ -36,6 +36,18 @@ export const registerContest=async (req, res) => {
     if (contest.registeredUsers.includes(req.user._id)) {
       return res.status(400).json({ message: 'User already registered for the contest' });
     }
+    
+    const {token} =req.cookies;
+    if(!token){
+        return res.status(404).json({
+            success:false,
+            message:"Login First"
+        })
+    }
+
+    const decoded=await jwt.verify(token,process.env.JWT_SECRET);
+    req.user=await User.findById(decoded._id);
+
 
     contest.registeredUsers.push(req.user._id);
     await contest.save();
